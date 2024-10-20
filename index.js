1ed043d1868284a4dee1bf2fe4f20a179cbfdd07
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2024-10-20 20:33:01 +0100

    go

---
+++ index.js
@@ -0,0 +1,19 @@
+let name = "Blessing";
+let age = 21;
+let school = "University of Nigeria";
+
+let isAfrican = false;
+
+const pi = 3.142;
+
+
+age = 25;
+
+isAfrican = true;
+
+
+
+console.log(name);
+console.log(age)
+console.log(school);
+console.log(isAfrican);
