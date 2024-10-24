b280bace26de565e2e91eab1e911e7b559efefa6
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2024-10-24 08:15:01 +0100

    go

---
+++ week1_day2/index.js
@@ -0,0 +1,116 @@
+// addition     +
+
+let firstNumber = 6;
+let secondNumber = 8;
+
+let sum = firstNumber + secondNumber;
+
+// console.log(sum);//14
+
+// console.log(4 + 8);//12
+
+// subtraction -
+let diff = secondNumber - firstNumber;
+// console.log(diff)//2
+
+// multiplication *
+let product = firstNumber * secondNumber
+
+// console.log(product);
+
+//exponatition **
+// 2 raise to power 2
+// 2 ** 2
+
+let num1 = 3;
+let num2 = 2;
+
+let expo = num2 ** num1;
+// console.log(expo);
+
+// division /
+// console.log(6 / 4);
+// console.log(45 / 9);
+
+// modulo %
+// console.log(5 % 3);
+
+
+//increment ++
+let age2023 = 20;
+age2023++;
+
+// console.log(age2023);
+
+// decrement --
+let age = 34;
+age--;
+
+// console.log(age);
+
+
+let name = "Ijeoma chukwu";
+// console.log(name.length);
+
+
+let firstName = "Chukwuemeka";
+let lastName = "Chukwuma";
+
+let fullName = firstName +" "+ lastName;
+// console.log(fullName);
+
+
+// assignment operator =
+
+let ageNow = 9;
+let nameOfTeacher = "Chijiuba"
+
+
+// comparism 
+
+// equal to ==
+
+let number1 = 8;
+let number2 = 9;
+
+// console.log(number1 === number2);
+
+
+// not equal to !=
+// console.log(number1 !== number2);
+
+
+// greater than >
+// less than < 
+// less than or equal to <=
+// greater than or equal to >= 
+// console.log(number1 <= number2);
+let herAge = 12
+if(herAge >= 18) {
+    console.log("She is an adult");
+}
+else if(herAge < 18) {
+    console.log("She is a child")
+}
+
+// loops
+// 1 2 3 4 5 6
+
+
+for(let i = 0; i < 5; i++) { //0
+    console.log("Chijiuba");
+}
+
+let x = 0;
+while (x < 5) {
+    console.log(x);
+    x++;
+}
+
+let n = 0
+
+
+do {
+    console.log("hello");
+    n++;
+} while(n < 5)
