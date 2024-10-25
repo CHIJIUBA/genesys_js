52cf259b0c70879eeb8dc13c43db100f2206f0e4
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2024-10-25 14:10:05 +0100

    go

--- week2_day1/index.js
+++ week2_day1/index.js
@@ -6,29 +6,8 @@
 function greet(name) {
 
     console.log("Hello " + name);
-    alert(name)
 
 }
-
-greet("Amara");
-
-// length * width
-
-function areaOfRectangle(length, width) {
-    let area = length * width;
-    return area;
-}
-
-
-let areaofectangle = areaOfRectangle(8, 5);
-
-
-let something = function() {
-    console.log("Hello World");
-}
-
-something();
-
 
 // let btn = document.querySelector("#click");
 
