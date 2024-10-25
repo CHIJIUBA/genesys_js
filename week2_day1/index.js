8bc73caa190641823feab441f36550d4c18cfe14
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2024-10-25 14:06:01 +0100

    go

---
+++ week2_day1/index.js
@@ -0,0 +1,37 @@
+// Functions In JavaScripts
+
+
+// function Examples
+
+function greet(name) {
+
+    console.log("Hello " + name);
+    alert(name)
+
+}
+
+greet("Amara");
+
+// length * width
+
+function areaOfRectangle(length, width) {
+    let area = length * width;
+    return area;
+}
+
+
+let areaofectangle = areaOfRectangle(8, 5);
+
+
+let something = function() {
+    console.log("Hello World");
+}
+
+something();
+
+
+// let btn = document.querySelector("#click");
+
+// btn.onclick = () => {
+//     console.log("You just clicked me");
+// }
